656a3a2ca9a6c05eecd409fe3e47b420dbd43346
unknown <bytesizedtrace@gmail.com>
2023-02-21 16:16:50 +1030

    Update locales module

--- modules/locales/main.lua
+++ modules/locales/main.lua
@@ -1,20 +1,13 @@
 local resourceLocales = {}
--- LoadData("locales", "locales/" .. Config.Locale .. ".json") or {}
 
-local locales = setmetatable({}, {
-    __index = function(self, k)
-        local invokingResource = getfenv(3)._RESOURCE
+return {
+    Fetch = function()
+        local invokingResource = getfenv(2)._RESOURCE
 
         if not resourceLocales[invokingResource] then
             resourceLocales[invokingResource] = LoadData(invokingResource, "locales/" .. Config.Locale .. ".json") or {}
         end
 
-        return resourceLocales[invokingResource][k]
+        return resourceLocales[invokingResource]
     end
-})
-
-Exports("_T", function(str)
-    return locales[str] or ""
-end)
-
-_ENV.Locales = locales
+}
