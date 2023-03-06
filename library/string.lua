bd28f5956be0234beca41050628d04dcf9bed276
BrianxTu <66759103+BrianxTu@users.noreply.github.com>
2023-03-06 16:45:59 -0500

    Update string.lua

--- library/string.lua
+++ library/string.lua
@@ -1,6 +1,23 @@
+--[[
+    string:byte
+    string:char
+    string:dump
+    string:find
+    string:format
+    string:gmatch
+    string:gsub
+    string:len
+    string:lower
+    string:match
+    string:rep
+    string:reverse
+    string:sub
+    string:upper
+]]
+
 function string:split(separator)
     local result = {}
-    pattern = string.format("([^%s]+)", separator)
+    pattern = separator:format("([^%s]+)")
     for token in self:gmatch(pattern) do
         table.insert(result, token)
     end
