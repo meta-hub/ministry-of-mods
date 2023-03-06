6369a2d0c9ad090e2147d7a57e4a927c37b73a0e
BrianxTu <66759103+BrianxTu@users.noreply.github.com>
2023-03-06 16:56:22 -0500

    Update string.lua

--- library/string.lua
+++ library/string.lua
@@ -1,25 +1,28 @@
---[[
-    string:byte
-    string:char
-    string:dump
-    string:find
-    string:format
-    string:gmatch
-    string:gsub
-    string:len
-    string:lower
-    string:match
-    string:rep
-    string:reverse
-    string:sub
-    string:upper
-]]
+-- string:byte (s [, i [, j]])
+-- string:char (···)
+-- string:dump (function)
+-- string:find (s, pattern [, init [, plain]])
+-- string:format (formatstring, ···)
+-- string:gmatch (s, pattern)
+-- string:gsub (s, pattern, repl [, n])
+-- string:len (s)
+-- string:lower (s)
+-- string:match (s, pattern [, init])
+-- string:rep (s, n)
+-- string:reverse (s)
+-- string:sub (s, i [, j])
+-- string:upper (s)
 
-function string:split(separator)
-    local result = {}
-    pattern = separator:format("([^%s]+)")
-    for token in self:gmatch(pattern) do
-        table.insert(result, token)
+function string:split(separator, useIter)
+    if useIter then
+        local pattern = separator:format("([^%s]+)")
+        return self:gmatch(pattern)
+    else
+        local result = {}
+        local pattern = separator:format("([^%s]+)")
+        for token in self:gmatch(pattern) do
+            table.insert(result, token)
+        end
+        return result
     end
-    return result
 end
